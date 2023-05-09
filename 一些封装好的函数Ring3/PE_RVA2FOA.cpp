f0be4e7275564ade59eb39d1a698c7619d32f6a9
槐拾柒 <120728667+Logic-Binary@users.noreply.github.com>
2023-05-09 11:38:03 +0800

    Add files via upload

---
+++ 一些封装好的函数Ring3/PE_RVA2FOA.cpp
@@ -0,0 +1,15 @@
+DWORD RVA2FOA(LPVOID buf, LPVOID RVA) {
+	PIMAGE_DOS_HEADER dos_header = PIMAGE_DOS_HEADER(buf);
+	PIMAGE_NT_HEADERS nt_header = PIMAGE_NT_HEADERS((DWORD)buf + dos_header->e_lfanew);
+	PIMAGE_FILE_HEADER file_header = PIMAGE_FILE_HEADER(&(nt_header->FileHeader));
+	PIMAGE_OPTIONAL_HEADER option_header = PIMAGE_OPTIONAL_HEADER(&(nt_header->OptionalHeader));
+	PIMAGE_SECTION_HEADER section_header = IMAGE_FIRST_SECTION(nt_header);
+
+	for (int i = 0; i < file_header->NumberOfSections; i++) {
+		if ((DWORD)RVA >= section_header->VirtualAddress &&
+			(DWORD)RVA < section_header->VirtualAddress + section_header->SizeOfRawData) {
+			return ((DWORD)RVA - section_header->VirtualAddress + section_header->PointerToRawData);
+		}
+		section_header++;
+	}
+}
